9a957a2fb065dab1e9e241bd4501ec00b617c81f
robin-raq <robin.raq@gmail.com>
2019-11-22 12:04:19 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
-const fullname = document.getElementById("fullname");
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
+fetch("https://randomuser.me/api/")
+  .then(r => r.json())
+  .then((personObj) => {
+    let personData = personObj.results[0]
+    console.log(personData)
+    const fullname = document.getElementById("fullname");
+    fullname.innerText = personData.name.first + " " + personData.name.last
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("CONTENT LOADED!");
-});
+    const email = document.getElementById("email")
+    email.innerText = personData.email
+
+    const street = document.getElementById("street")
+    street.innerText = personData.location.street.number + " " + personData.location.street.name
+
+    const city = document.getElementById("city")
+    city.innerText = personData.location.city
+
+    const state = document.getElementById("state")
+    state.innerText = personData.location.state
+
+    const postcode = document.getElementById("postcode")
+    postcode.innerText = personData.location.postcode
+
+    const phone = document.getElementById("phone")
+    phone.innerText = personData.phone
+
+    const cell= document.getElementById("cell")
+    cell.innerText = personData.cell
+
+    const dob = document.getElementById("date_of_birth")
+    dob.innerText = personData.dob.date
+
+  })
