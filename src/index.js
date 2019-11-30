d23f877ca63539d8d8b61377e5bf94c24d47453c
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-11-30 16:39:28 -0500

    done

--- src/index.js
+++ src/index.js
@@ -4,3 +4,58 @@ console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("CONTENT LOADED!");
 });
+
+
+const fetchData = () =>{
+  let button = document.querySelector('.btn')
+  
+  button.addEventListener('click', ( e ) =>{
+    fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(data => {
+      data.results.forEach(result => {
+        renderData(result)
+      });
+    })
+  })
+}
+
+fetchData()
+
+
+const renderData = (resultsObj) =>  {
+
+  let streetData = document.querySelector('#street')
+  streetData.innerText = `${resultsObj.location.street.number} ${resultsObj.location.street.name}`
+
+  let infoData = document.querySelector('#city')
+  infoData.innerText = resultsObj.location.city
+
+  let stateData = document.querySelector('#state')
+  stateData.innerText = resultsObj.location.state
+
+  let postCodeData = document.querySelector('#postcode')
+  postCodeData.innerText = resultsObj.location.postcode
+
+
+  let nameData = document.querySelector('#fullname')
+  nameData.innerText = `${resultsObj.name.first} ${resultsObj.name.last} `
+
+
+
+  let phoneData = document.querySelector('#phone')
+  phoneData.innerText = resultsObj.phone
+
+  let cellData = document.querySelector('#cell')
+  cellData.innerText = resultsObj.cell
+
+  let emailData = document.querySelector('#email')
+  emailData.innerText = resultsObj.email
+
+  let birthData = document.querySelector('#date_of_birth')
+  birthData.innerText = resultsObj.dob.date
+  debugger
+
+}
+
+
